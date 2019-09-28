faf0c459ef44e0e80ae2a8a9f0b615dc90e19986
Kal <info@kalacademy.org>
2019-09-28 12:19:40 -0700

    Clean up

--- BankApp/Program.cs
+++ BankApp/Program.cs
@@ -6,23 +6,6 @@ namespace BankApp
     {
         static void Main(string[] args)
         {
-            //Instance of an account == object
-            var account = Bank.CreateAccount("test@test.com", 
-                TypeOfAccounts.Checking, 245);
-
-            Console.WriteLine($"AN: {account.AccountNumber}, " +
-                $"Balance: {account.Balance:C}, " +
-                $"AT: {account.AccountType}, " +
-                $"EA:{account.EmailAddress}, " +
-                $"CD: {account.CreatedDate}");
-
-            var account2 = Bank.CreateAccount("test1@test.com",
-                TypeOfAccounts.Savings, 0);
-            Console.WriteLine($"AN: {account2.AccountNumber}, " +
-                $"Balance: {account2.Balance}, " +
-                $"AT: {account2.AccountType}, " +
-                $"EA:{account2.EmailAddress}, " +
-                $"CD: {account2.CreatedDate}");
 
         }
     }
